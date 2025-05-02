0c3b5890a0ea00ee023338163dbdf8dc948ce5a8
Thomas James <7homasjames@gmail.com>
2025-05-02 20:52:28 +0530

    Update app8.py

--- app8.py
+++ app8.py
@@ -3,7 +3,7 @@ import requests
 import pdfplumber
 import docx2txt
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://resumhunter-v2.onrender.com"
 
 def ats_check(resume_text, job_description, managers_note=""):
     try:
